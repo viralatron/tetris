d128b028cb100d4a3c692b76ca1c4b5461734909
Daniel Amaral e Silva <dan.amaralsilva@gmail.com>
2021-11-15 01:25:19 -0300

    first commit

---
+++ src/components/Cell.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
+
+const Cell = ({ type }) => (
+  <StyledCell type={"L"} color={TETROMINOS["L"].color}>
+    cell
+  </StyledCell>
+);
+
+export default Cell;
